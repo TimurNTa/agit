052b9336a4045a6f9457d0e4059aaf0c4b88b145
TimurNTa <nazimovtimur216@gmail.com>
2026-08-17 18:08:56 +0300

    ux: support worker location on task map

--- src/components/map/MapClient.tsx
+++ src/components/map/MapClient.tsx
@@ -5,6 +5,11 @@ import type { MapTask } from "@/components/map/types";
 
 const AgitMap = dynamic(() => import("./AgitMap").then((mod) => mod.AgitMap), { ssr: false });
 
-export function MapClient(props: { tasks: MapTask[]; selectedId?: string; onSelect: (id: string) => void }) {
+export function MapClient(props: {
+  tasks: MapTask[];
+  selectedId?: string;
+  onSelect: (id: string) => void;
+  userLocation?: { lat: number; lon: number };
+}) {
   return <AgitMap {...props} />;
 }
